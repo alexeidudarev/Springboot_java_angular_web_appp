d3f93b571b2335fd9bd3cb1b7ac81b2f5dc84900
Alexei Dudarev <alexei.dudarev@gmail.com>
2021-09-28 14:37:53 +0300

    error component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
+  {path:'**',component:ErrorComponent},
   {path:'login',component:LoginComponent},
   {path:'welcome',component:WelcomeComponent}
 ];
